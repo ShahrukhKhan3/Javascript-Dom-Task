fd848e2b4fecc20020518855a262e9c5de806aa1
ShahrukhKhan3 <sk3546841@gmail.com>
2022-09-25 23:51:39 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelector('.click').addEventListener('click',function(){
+
+    document.body.style.backgroundColor='red';
+
+document.querySelector('.input').value=4;
+
+
+document.querySelector('.input').style.width='200px';
+
+
+
+})
+
+document.querySelector('.heading').textContent='Hello';
